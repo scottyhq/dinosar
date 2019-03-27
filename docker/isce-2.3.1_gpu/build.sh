358189fd4b462cac83a40bab5fc2697262bb8f5b
Craig Stringham <craig@capellaspace.com>
2019-03-27 13:23:26 -0700

    restoring deletion of intermediate docker images

--- docker/isce-2.3.1_gpu/build.sh
+++ docker/isce-2.3.1_gpu/build.sh
@@ -3,6 +3,6 @@ echo '\n***** Downloading ISCE from github... *****\n'
 wget https://github.com/isce-framework/isce2/archive/v2.3.1.tar.gz
 echo '\n***** Starting Docker multistage build *****\n'
 docker build --rm -t dinosar/isce:v2.3.1_gpu . 
-#echo '\n***** Removing intermediate docker image *****\n'
-#docker rmi $(docker images -q -f dangling=true)
+echo '\n***** Removing intermediate docker image *****\n'
+docker rmi $(docker images -q -f dangling=true)
 echo '\n***** ISCE is now a Docker image tagged "dinosar/isce:v2.3.1_gpu" *****\n'
