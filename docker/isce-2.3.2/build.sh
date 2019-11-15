449f0f10ae51a95d6a40f5a910536310c874655e
Scott Henderson <scottyhq@gmail.com>
2019-11-15 14:06:11 -0800

    added isce2.3.2 docker build folder

---
+++ docker/isce-2.3.2/build.sh
@@ -0,0 +1,8 @@
+# Script to retrive ISCE source code and create a docker container
+echo '\n***** Downloading ISCE from github... *****\n'
+wget https://github.com/isce-framework/isce2/archive/v2.3.2.tar.gz
+echo '\n***** Starting Docker multistage build *****\n'
+docker build --rm -t dinosar/isce:v2.3.2 . 
+echo '\n***** Removing intermediate docker image *****\n'
+docker rmi $(docker images -q -f dangling=true)
+echo '\n***** ISCE is now a Docker image tagged "dinosar/isce:v2.3.2" *****\n'
