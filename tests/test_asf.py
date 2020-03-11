b3063676a23a2fac06de0190a3ce4d691ccf4bbb
Scott Henderson <scottyhq@gmail.com>
2020-03-11 16:01:15 -0700

    fix formatting

--- tests/test_asf.py
+++ tests/test_asf.py
@@ -6,6 +6,7 @@ import os
 import geopandas as gpd
 import contextlib
 
+
 @contextlib.contextmanager
 def run_in(path):
     CWD = os.getcwd()
@@ -13,9 +14,11 @@ def run_in(path):
     try:
         yield
     except Exception as e:
+        print(e)
         raise
     finally:
         os.chdir(CWD)
+
 
 @pytest.mark.network
 def test_query_asf(tmpdir):
@@ -24,6 +27,7 @@ def test_query_asf(tmpdir):
         asf.query_asf([0.611, 1.048, -78.196, -77.522], "S1B")
         assert os.path.isfile("query_S1A.json")
         assert os.path.isfile("query_S1B.json")
+
 
 @pytest.mark.network
 def test_get_list():
@@ -39,6 +43,7 @@ def test_get_list():
     print(r.status_code)
     assert r.status_code == 200
 
+
 def test_ogr2swe():
     bounds = asf.ogr2snwe("tests/data/UnionGap.shp")
     assert type(bounds) == list
@@ -49,6 +54,7 @@ def test_ogr2swe():
         -120.471510549134,
         -120.4502650392162,
     ]
+
 
 def test_load_asf_json():
     gf = asf.load_asf_json("tests/data/query_S1A.json")
@@ -63,6 +69,7 @@ def test_merge_inventories():
 def test_load_inventory():
     gf = asf.load_inventory("tests/data/query.geojson")
     assert type(gf) == gpd.geodataframe.GeoDataFrame
+
 
 @pytest.mark.network
 def test_get_orbit_url():
@@ -95,6 +102,7 @@ def test_get_slc_urls():
 S1B_IW_SLC__1SDV_20180320T232821_20180320T232848_010121_01260A_0613.zip"
     )
 
+
 def test_get_slc_names():
     acquisition_date = "20180320"
     path = 120
@@ -102,34 +110,37 @@ def test_get_slc_names():
     names = asf.get_slc_names(gf, acquisition_date, path)
     assert type(names) == list
     assert len(names) == 1
-    assert names[0] == "S1B_IW_SLC__1SDV_20180320T232821_20180320T232848_010121_01260A_0613.zip"
+    assert (
+        names[0]
+        == "S1B_IW_SLC__1SDV_20180320T232821_20180320T232848_010121_01260A_0613.zip"
+    )
+
 
 def test_summarize_orbits(tmpdir):
     gf = asf.load_inventory("tests/data/query.geojson")
     with run_in(tmpdir):
         asf.summarize_orbits(gf)
-        assert os.path.isfile('acquisitions_40.csv')
+        assert os.path.isfile("acquisitions_40.csv")
 
 
 def test_save_geojson_footprints(tmpdir):
     gf = asf.load_inventory("tests/data/query.geojson")
     with run_in(tmpdir):
         asf.save_geojson_footprints(gf)
-        assert os.path.isfile('40/2015-10-03.geojson')
+        assert os.path.isfile("40/2015-10-03.geojson")
 
 
 def test_save_inventory(tmpdir):
     gf = asf.load_inventory("tests/data/query.geojson")
     with run_in(tmpdir):
-        asf.save_inventory(gf, outname='test.geojson')
-        assert os.path.isfile('test.geojson')
+        asf.save_inventory(gf, outname="test.geojson")
+        assert os.path.isfile("test.geojson")
 
 
 def test_snwe2file(tmpdir):
     snwe = [0.611, 1.048, -78.196, -77.522]
-
     with run_in(tmpdir):
-        gf = asf.snwe2file(snwe)
-        assert os.path.isfile('snwe.json')
-        assert os.path.isfile('snwe.wkt')
-        assert os.path.isfile('snwe.txt')
+        asf.snwe2file(snwe)
+        assert os.path.isfile("snwe.json")
+        assert os.path.isfile("snwe.wkt")
+        assert os.path.isfile("snwe.txt")
